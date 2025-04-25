ba83d090f889f0417a9c719258a33cd0e29d9350
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-25 12:28:03 +0530

    current smaller optimal

--- currentsmaller.cpp
+++ currentsmaller.cpp
@@ -16,4 +16,42 @@ public:
         
         return result;
     }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+using namespace std;
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> count(101, 0);
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            count[nums[i]]++;
+        }
+
+        for (int i = 1; i < 101; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0)
+                ans.push_back(0);
+            else
+                ans.push_back(count[nums[i] - 1]);
+        }
+
+        return ans;
+    }
 };
